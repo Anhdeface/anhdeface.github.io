3e55017e28b9d27160adda19098d4ed9f52bb2b5
KKT <133498656+Anhdeface@users.noreply.github.com>
2024-01-28 07:25:56 +0700

    Update ALL.cpp

--- sourcecode-xamlul/ALL.cpp
+++ sourcecode-xamlul/ALL.cpp
@@ -49,3 +49,35 @@ for(int i=a;i<b;i++){
 cout<<sum;
 }
 //source 4
+#include<bits/stdc++.h>
+using namespace std;
+int dec (string s) {
+int result=0,so=0;
+for(int i=0;i<s.size();i++){
+    if(s[i]>='0'&&s[i]<='9'){
+        so = s[i] -'0';
+        if((so%3==0)&&(so%2==0))
+        result+=3;
+        if (so%3!=0)
+            result+=2;
+         if ((so%2!=0)&&(so%3==0))
+            result+=1;
+         if(so==0)
+            result+=4;
+        so =0;
+    }
+}
+return result;
+}
+int main () {
+    int n;
+    string code[1000];
+    cin>>n;
+    cin.ignore();
+    for(int i=0;i<n;i++)
+        getline(cin,code[i]);
+
+    for(int j=0;j<n;j++)
+        cout<<dec(code[j])<<endl;
+}
+// source 5
