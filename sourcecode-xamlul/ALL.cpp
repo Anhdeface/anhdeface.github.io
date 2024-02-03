8ec7d693b1b4b4253999bccc1465b03c9bc42d56
KKT <133498656+Anhdeface@users.noreply.github.com>
2024-02-03 19:48:53 +0700

    Update ALL.cpp

--- sourcecode-xamlul/ALL.cpp
+++ sourcecode-xamlul/ALL.cpp
@@ -85,3 +85,16 @@ for(int i=2;i<x;i++){
     if(x%i==0)
     return false;
 // source 6
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int a,b;
+    cin>>a>>b;
+    for(int i=__gcd(a,b);i>=1;i--){
+        if(i!=__gcd(a,b)&&a%i==0&&b%i==0)
+            {cout<<i<<endl;break;}
+
+    }
+
+}
+// source 7
